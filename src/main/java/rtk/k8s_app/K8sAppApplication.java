9c76e8982a4e539d11d72e00cbb0d712f615bc82
release_the_kraken <wmichal13@wp.pl>
2025-03-05 18:52:10 +0100

    initial commit

---
+++ src/main/java/rtk/k8s_app/K8sAppApplication.java
@@ -0,0 +1,13 @@
+package rtk.k8s_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rtk/k8s_app/K8sAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rtk.k8s_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
